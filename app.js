a098dc165263322ec89aa5ae89efa684afe5004f
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-25 12:57:44 +0100

    added some const variable in the app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const computerChoice = document.getElementById("computer-choice");
+const userChoice = document.getElementById("user-choice");
+const result = document.getElementById("result");
+
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+const rockButton = document.getElementById("rock");
