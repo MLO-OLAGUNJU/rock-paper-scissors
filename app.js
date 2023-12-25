60c20d618218a3820e5e0708fac69e362bcf7910
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-25 13:05:16 +0100

    added some scripts logs and modified index.html

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const computerChoice = document.getElementById("computer-choice");
-const userChoice = document.getElementById("user-choice");
-const result = document.getElementById("result");
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("user-choice");
+const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+let userChoice;
+
+possibleChoices.forEach((possibleChoice) =>
+  possibleChoice.addEventListener("click", () => {
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+  })
+);
 
 const paperButton = document.getElementById("paper");
 const scissorsButton = document.getElementById("scissors");
